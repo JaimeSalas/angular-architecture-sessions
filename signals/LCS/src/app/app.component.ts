cb573908eb1fe86d8e8aeddb5b92ca3ae291de74
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2024-02-22 13:19:41 +0100

    added examples

--- signals/LCS/src/app/app.component.ts
+++ signals/LCS/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Subscription, of, from, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,46 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   pageTitle = 'Lemoncode Store';
   cartCount = 0;
+
+  sub!: Subscription;
+  subArray!: Subscription;
+  subFrom!: Subscription;
+  subEvent!: Subscription;
+
+  ngOnInit(): void {
+    this.sub = of(2, 4, 6, 8).subscribe((item) =>
+      console.log(`Value from of: `, item)
+    );
+    this.subArray = of([2, 4, 6, 8]).subscribe((item) =>
+      console.log(`Value from of (array): `, item)
+    );
+    this.subFrom = from([20, 15, 10, 5]).subscribe({
+      next(item) {
+        console.log('from', item);
+      },
+      error(err) {
+        console.log(err);
+      },
+      complete() {
+        console.log('completed');
+      },
+    });
+    this.subEvent = fromEvent(document, 'click').subscribe({
+      next: (ev) => {
+        console.log(ev.target);
+      },
+      error: (err) => console.log(err),
+      complete: () => console.log('complet'),
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+    this.subArray.unsubscribe();
+    this.subFrom.unsubscribe();
+    this.subEvent.unsubscribe();
+  }
 }
